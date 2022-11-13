0a5ed41b1eddbf1b65078e44f2b48e6e00b9af64
maaz-afridi20 <maazafridi.ma47@gmail.com>
2022-11-13 22:20:46 +0500

    unfocus text field when we click on epmty space

--- lib/addpage.dart
+++ lib/addpage.dart
@@ -20,121 +20,126 @@ class _AddPageState extends State<AddPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Add Page'),
-      ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Center(
-            child: Form(
-              key: formkey,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const SizedBox(height: 20),
-                    TextFormField(
-                      controller: titleC,
-                      decoration: InputDecoration(
-                        labelText: 'Title',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Add Page'),
+        ),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Center(
+              child: Form(
+                key: formkey,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        controller: titleC,
+                        decoration: InputDecoration(
+                          labelText: 'Title',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Enter title';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 10),
+                      TextFormField(
+                        maxLines: 7,
+                        minLines: 1,
+                        controller: detailC,
+                        decoration: InputDecoration(
+                          labelText: 'Detail',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Enter details';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        controller: collectionC,
+                        decoration: InputDecoration(
+                          labelText: 'Collection Name',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Enter Collection Name';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        style: ButtonStyle(
+                          minimumSize: MaterialStateProperty.all(
+                            const Size(double.infinity, 60),
+                          ),
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.deepOrange),
+                        ),
+                        onPressed: () {
+                          // Fluttertoast.showToast(msg: 'You pressed button');
+                          if (formkey.currentState!.validate()) {
+                            DataBaseServices.addData(
+                                titleC.text, detailC.text, collectionC.text);
+                            titleC.clear();
+                            detailC.clear();
+                            collectionC.clear();
+                          }
+                        },
+                        child: const Text(
+                          'Add Data',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
                         ),
                       ),
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Enter title';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 10),
-                    TextFormField(
-                      maxLines: 7,
-                      minLines: 1,
-                      controller: detailC,
-                      decoration: InputDecoration(
-                        labelText: 'Detail',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      ElevatedButton(
+                        style: ButtonStyle(
+                          minimumSize: MaterialStateProperty.all(
+                            const Size(double.infinity, 60),
+                          ),
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.deepOrange),
+                        ),
+                        onPressed: () {
+                          Navigationss.navigator(
+                            context,
+                            ShowData(),
+                          );
+                        },
+                        child: const Text(
+                          'Show Data',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
                         ),
                       ),
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Enter details';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 20),
-                    TextFormField(
-                      controller: collectionC,
-                      decoration: InputDecoration(
-                        labelText: 'Collection Name',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Enter Collection Name';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      style: ButtonStyle(
-                        minimumSize: MaterialStateProperty.all(
-                          const Size(double.infinity, 60),
-                        ),
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.deepOrange),
-                      ),
-                      onPressed: () {
-                        // Fluttertoast.showToast(msg: 'You pressed button');
-                        if (formkey.currentState!.validate()) {
-                          DataBaseServices.addData(
-                              titleC.text, detailC.text, collectionC.text);
-                          titleC.clear();
-                          detailC.clear();
-                          collectionC.clear();
-                        }
-                      },
-                      child: const Text(
-                        'Add Data',
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    ElevatedButton(
-                      style: ButtonStyle(
-                        minimumSize: MaterialStateProperty.all(
-                          const Size(double.infinity, 60),
-                        ),
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.deepOrange),
-                      ),
-                      onPressed: () {
-                        Navigationss.navigator(
-                          context,
-                          ShowData(),
-                        );
-                      },
-                      child: const Text(
-                        'Show Data',
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
